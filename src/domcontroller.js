cac2e5942ba359e1a51c7a0c97ad43286b23691d
magic-onion <alexander.m.truong@gmail.com>
2018-12-07 11:10:19 -0500

    added a domcontroller method

--- src/domcontroller.js
+++ src/domcontroller.js
@@ -3,16 +3,21 @@ class DomController {
     this.main = document.querySelector('.main')
     this.scoreboard = document.querySelector('#scoreboard')
 
-    
+
 
     //need a click event to play a game
   }
-
   appendToMain(array) {
     array.forEach(html => this.main.appendChild(html))
   }
 
-
+  setScore(array) {
+    array.forEach(scoreItem => {
+      let score = document.createElement('li')
+      score.innerText = `${scoreItem}`
+      this.scoreboard.appendchild(score)
+    })
+  }
 
 
 }
